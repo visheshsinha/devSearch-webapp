99ea96260eec5bbabd2d3ddb530409e903455328
visheshsinha <visheshsinha801@gmail.com>
2022-02-04 01:24:26 +0530

    Templates and Template Integration

--- devSearch/projects/views.py
+++ devSearch/projects/views.py
@@ -3,8 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+
 def projects(request):
-    return HttpResponse("This is our projects page")
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse(f'ProjectPage: {pk}')
+    return render(request, 'projects/single_projects.html')
