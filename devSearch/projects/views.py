c09bb9749511722ff73b079311f989ad11c6ab29
visheshsinha <visheshsinha801@gmail.com>
2022-02-06 20:40:27 +0530

    Database Queries

--- devSearch/projects/views.py
+++ devSearch/projects/views.py
@@ -1,36 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website',
-        'top_rated': True
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work',
-        'top_rated': False
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community',
-        'top_rated': True
-    }
-]
+from .models import Project
 
 
 def projects(request):
-    context = {"projects":projectsList}
-    return render(request, 'projects/projects.html', context)
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "projects/projects.html", context)
+
 
 def project(request, pk):
-    projectObject = None
-    for project in projectsList:
-        if pk == project["id"]:
-            projectObject = project
-
-    return render(request, 'projects/single_projects.html', {"project":projectObject})
+    projectObject = Project.objects.get(id=pk)
+    return render(request, "projects/single_projects.html", {"project": projectObject})
